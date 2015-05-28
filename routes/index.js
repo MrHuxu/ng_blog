dd896a6fae2c5e05ab3c2b6a51796525825f016c
Xu Hu <hxtheone@gmail.com>
2015-05-29 01:44:55 +0800

    detect the enviroment

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'life of xhu' });
+  console.log(process.env.NODE_ENV);
+  switch (process.env.NODE_ENV) {
+    case 'development':
+      res.render('index_dev');
+    case 'production':
+      res.render('index_prd');
+    default:
+      res.render('index_dev');
+  }
 });
 
 module.exports = router;
