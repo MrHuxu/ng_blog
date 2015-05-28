2928f60f32b700b7cc7659799f3e8c92ba63e6ee
xhu <xhu@freewheel.tv>
2015-05-28 18:31:53 +0800

    update gulpfile.js to compress js and css

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,32 @@
 var gulp = require('gulp'),
-    nodemon = require('gulp-nodemon');
+    nodemon = require('gulp-nodemon'),
+    minifyCss = require('gulp-minify-css'),
+    concat = require('gulp-concat'),
+    concatCss = require('gulp-concat-css'),
+    uglify = require('gulp-uglify'),
+    rename = require('gulp-rename');
+
+gulp.task('js', function () {
+  gulp.src([
+      './public/javascripts/blog_module.js', 
+      './public/javascripts/*.js'
+    ])
+    .pipe(concat('all.min.js'))
+    .pipe(gulp.dest('./public/dist'))
+    .pipe(uglify({mangle: false}))
+    .pipe(gulp.dest('./public/dist'));
+});
+
+gulp.task('css', function () {
+  gulp.src('./public/stylesheets/*.css')
+    .pipe(concatCss('all.min.css'))
+    .pipe(minifyCss({compatibility: 'ie8'}))
+    .pipe(gulp.dest('./public/dist'));
+})
+
+gulp.task('compress', function () {
+  gulp.run('js', 'css');
+})
 
 gulp.task('develop', function () {
   nodemon({
